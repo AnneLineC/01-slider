ed815c91319f7b29a407e57beee9d66d2619e1d6
AnneLineC <anneline.col@gmail.com>
2021-08-14 11:22:23 +0200

    Défilement par flèches du Slider gérées en JS

--- js/app.js
+++ js/app.js
@@ -1,23 +1,58 @@
-const sliderRightArrow = document.querySelector('.slider__arrow--right');
-sliderRightArrow.addEventListener('click', handleClickRightArrow);
+const app = {
+    currentSlide: 1,
+    init: function() {
+
+        console.log(app.currentSlide);
+
+        const sliderRightArrow = document.querySelector('.slider__arrow--right');
+        sliderRightArrow.addEventListener('click', app.handleClickRightArrow);
+
+        const sliderLeftArrow = document.querySelector('.slider__arrow--left');
+        sliderLeftArrow.addEventListener('click', app.handleClickLeftArrow);
 
 
-function handleClickRightArrow() {
+    },
 
-    const activeSlide = document.querySelector('.slider__slide--active');
-    const nextActiveSlide = activeSlide.nextElementSibling;
-    activeSlide.classList.remove('slider__slide--active');
-    nextActiveSlide.classList.add('slider__slide--active');
-    console.log(activeSlide);
-    console.log(nextActiveSlide);
+    handleClickRightArrow: function() {
+        app.currentSlide++;
+        
+        const nbSlides = document.querySelectorAll('.slider__slide').length;
+        console.log(app.currentSlide + ' sur ' + nbSlides);
+    
+        const activeSlide = document.querySelector('.slider__slide--active');
+        activeSlide.classList.remove('slider__slide--active');
+    
+        if (app.currentSlide <= nbSlides) {
+            const nextActiveSlide = activeSlide.nextElementSibling;
+            nextActiveSlide.classList.add('slider__slide--active');
+        } else {
+            app.currentSlide = 1;
+            console.log(app.currentSlide);
+            const firstSlide = document.getElementById('slide-' + app.currentSlide);
+            firstSlide.classList.add('slider__slide--active');
+        }
+    },
 
-
-    // const slides = document.querySelectorAll('.slider__slide');
-    // // console.log(slides);
-    // for (const slide of slides) {
-    //     slide.classList.remove('slider__slide--active');
-    //     // console.log(slide);
-    // }
-
-
+    handleClickLeftArrow: function() {
+        app.currentSlide--;
+    
+        const nbSlides = document.querySelectorAll('.slider__slide').length;
+        console.log(app.currentSlide + ' sur ' + nbSlides);
+    
+        const activeSlide = document.querySelector('.slider__slide--active');
+        activeSlide.classList.remove('slider__slide--active');
+    
+        if (app.currentSlide >= 1) {
+            const nextActiveSlide = activeSlide.previousElementSibling;
+            nextActiveSlide.classList.add('slider__slide--active');
+        } else {
+            app.currentSlide = 4;
+            console.log(app.currentSlide);
+            const firstSlide = document.getElementById('slide-' + app.currentSlide);
+            firstSlide.classList.add('slider__slide--active');
+        }
+    
+    },
 }
+
+document.addEventListener('DOMContentLoaded', app.init);
